ffe3f212e8a6b6196c0a0f6a33d7553650c657ea
jangseyeong <jang.se.yeong000@gmail.com>
2024-11-16 00:08:07 +0900

    feat: sidebar 추가

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -9,8 +9,11 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { Calendar, Home, Search } from 'lucide-react';
+
 import Link from 'next/link';
+import { RiRobot2Fill } from 'react-icons/ri';
+import { TbMessageChatbot } from 'react-icons/tb';
+import { Home, LucideLogOut, Search } from 'lucide-react';
 
 const items = [
   {
@@ -26,14 +29,29 @@ const items = [
   {
     title: 'Interview',
     url: '/interview',
-    icon: Calendar,
+    icon: TbMessageChatbot,
   },
 ];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <RiRobot2Fill className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="body3">HIRO</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Resume</SidebarGroupLabel>
@@ -53,7 +71,16 @@ export function AppSidebar() {
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem>내 프로필</SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <button>
+                  <LucideLogOut />
+                  <span>Logout</span>
+                </button>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
