e86befcc8a94199bdfcd93d460aed985c4231a24
jangseyeong <jang.se.yeong000@gmail.com>
2024-11-16 08:33:45 +0900

    feat: 이력서 가져오는거 useQuery로 변경

--- src/app/(main)/_components/AllResumeList.tsx
+++ src/app/(main)/_components/AllResumeList.tsx
@@ -1,38 +1,32 @@
 'use client';
 
+import MessageText from '@/components/common/MessageText';
+import Spinner from '@/components/common/Spinner';
 import ResumeCard from '@/components/resume/ResumeCard';
-import { resumeService } from '@/services/resumeService';
-import type { Resume } from '@/types/resumeType';
-import { useEffect, useState } from 'react';
+import { useGetAllResume } from '@/hooks/queries/useResumeService';
+import { useEffect } from 'react';
 
 interface AllResumeList {
   selectPdfUrl: (pdfUrl: string) => void;
+  setFileCount: (count: number) => void;
 }
-const AllResumeList = ({ selectPdfUrl }: AllResumeList) => {
-  const [resumes, setResumes] = useState<Resume[]>([]);
-
-  useEffect(() => {
-    const fetchResumes = async () => {
-      try {
-        const data = await resumeService.getAllResume();
-        console.log('Fetched resumes:', data);
-        setResumes(data);
-      } catch (error) {
-        console.error('Error fetching resumes:', error);
-      }
-    };
-
-    fetchResumes();
-  }, []);
-
-  useEffect(() => {
-    console.log('Resumes state updated:', resumes);
-  }, [resumes]);
-
+const AllResumeList = ({ selectPdfUrl, setFileCount }: AllResumeList) => {
+  const { data, isLoading, isError, isSuccess } = useGetAllResume();
+  console.log(data);
   const testResume = [
     { resumeId: 'resume1', fileName: '이름입니다.pdf', fileSize: '33MB', selectPdfUrl },
     { resumeId: 'resume2', fileName: '이름입니다.pdf', fileSize: '33MB', selectPdfUrl },
   ];
+  useEffect(() => {
+    if (isSuccess && data) {
+      setFileCount(data.length);
+    }
+  }, [data, isSuccess, setFileCount]);
+  if (isError)
+    return <MessageText className="mt-20" message="이력서를 불러오는 중 오류가 발생했습니다." />;
+  if (isLoading) return <Spinner className="mt-20 w-full" />;
+  if (data?.length === 0)
+    return <MessageText className="mt-20" message="업로드한 이력서가 없습니다." />;
   return (
     <ul className="flex flex-col gap-y-4">
       {testResume.map((test) => (
--- src/hooks/queries/useResumeService.ts
+++ src/hooks/queries/useResumeService.ts
@@ -1,14 +1,12 @@
-// import { AxiosError } from 'axios';
-// import { useSuspenseQuery } from '@tanstack/react-query';
-// import { resumeService } from '@/services/resumeService';
-// import type { Resume } from '@/types/resumeType';
+import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { resumeService } from '@/services/resumeService';
+import type { Resume } from '@/types/resumeType';
 
-// // 이력서 전체 조회
-// // export const useGetAllResume = () => {
-// //   return useSuspenseQuery<Resume[], AxiosError>({
-// //     queryKey: ['getAllResume'],
-// //     queryFn: () => {
-// //       return !window ? [] : resumeService.getAllResume();
-// //     },
-// //   });
-// // };
+// 이력서 전체 조회
+export const useGetAllResume = () => {
+  return useQuery<Resume[], AxiosError>({
+    queryKey: ['getAllResume'],
+    queryFn: () => resumeService.getAllResume(),
+  });
+};
