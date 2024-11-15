80d04d7e0b1b3bbebb7d1eea8df69451dec2a4fe
jangseyeong <jang.se.yeong000@gmail.com>
2024-11-16 05:09:41 +0900

    feat: 파일 일괄 업로드 기능 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,28 +6,24 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 
 import ResumeCard from '@/components/resume/ResumeCard';
 import ResumePreview from '@/components/resume/ResumePreview';
+import UploadResumeDialog from './_components/UploadResumeDialog';
 
 export default function HomePage() {
   const [pdfUrl] = useState<string | null>(null);
   const testResume = [
     { resumeId: 'resume1', fileName: '이름입니다.pdf', fileSize: '33MB' },
     { resumeId: 'resume2', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume3', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume4', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume5', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume6', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume7', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume8', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume9', fileName: '이름입니다.pdf', fileSize: '33MB' },
-    { resumeId: 'resume10', fileName: '이름입니다.pdf', fileSize: '33MB' },
   ];
+
   return (
     <div className="flex h-[calc(100vh-65px)] flex-col gap-y-3 p-4">
-      <div className="flex items-end justify-between">
+      <div className="flex flex-shrink-0 items-end justify-between">
         <h2>현재 업로드한 파일 수: 100</h2>
-        <Button variant="outline">이력서 업로드 하기</Button>
+        <UploadResumeDialog>
+          <Button variant="outline">이력서 업로드 하기</Button>
+        </UploadResumeDialog>
       </div>
-      <div className="grid grid-cols-2 gap-4 overflow-hidden">
+      <div className="grid flex-grow grid-cols-2 gap-4 overflow-hidden">
         <ScrollArea className="h-full pr-2">
           <ul className="flex flex-col gap-y-4">
             {testResume.map((test) => (
