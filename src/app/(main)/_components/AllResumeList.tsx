2b3ed76a4534aa9fe41bd82d656965ba2104a3b6
JANGSEYEONG <jang.se.yeong000@gmail.com>
2024-11-15 23:22:16 +0000

    fix: 빌드 오류 임시 수정

--- src/app/(main)/_components/AllResumeList.tsx
+++ src/app/(main)/_components/AllResumeList.tsx
@@ -1,13 +1,34 @@
 'use client';
+
 import ResumeCard from '@/components/resume/ResumeCard';
-import { useGetAllResume } from '@/hooks/queries/useResumeService';
+import { resumeService } from '@/services/resumeService';
+import type { Resume } from '@/types/resumeType';
+import { useEffect, useState } from 'react';
 
 interface AllResumeList {
   selectPdfUrl: (pdfUrl: string) => void;
 }
 const AllResumeList = ({ selectPdfUrl }: AllResumeList) => {
-  const { data } = useGetAllResume();
-  console.log(data);
+  const [resumes, setResumes] = useState<Resume[]>([]);
+
+  useEffect(() => {
+    const fetchResumes = async () => {
+      try {
+        const data = await resumeService.getAllResume();
+        console.log('Fetched resumes:', data);
+        setResumes(data);
+      } catch (error) {
+        console.error('Error fetching resumes:', error);
+      }
+    };
+
+    fetchResumes();
+  }, []);
+
+  useEffect(() => {
+    console.log('Resumes state updated:', resumes);
+  }, [resumes]);
+
   const testResume = [
     { resumeId: 'resume1', fileName: '이름입니다.pdf', fileSize: '33MB', selectPdfUrl },
     { resumeId: 'resume2', fileName: '이름입니다.pdf', fileSize: '33MB', selectPdfUrl },
--- src/hooks/queries/useResumeService.ts
+++ src/hooks/queries/useResumeService.ts
@@ -1,12 +1,14 @@
-import { AxiosError } from 'axios';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { resumeService } from '@/services/resumeService';
-import type { Resume } from '@/types/resumeType';
+// import { AxiosError } from 'axios';
+// import { useSuspenseQuery } from '@tanstack/react-query';
+// import { resumeService } from '@/services/resumeService';
+// import type { Resume } from '@/types/resumeType';
 
-// 이력서 전체 조회
-export const useGetAllResume = () => {
-  return useSuspenseQuery<Resume[], AxiosError>({
-    queryKey: ['getAllResume'],
-    queryFn: () => resumeService.getAllResume(),
-  });
-};
+// // 이력서 전체 조회
+// // export const useGetAllResume = () => {
+// //   return useSuspenseQuery<Resume[], AxiosError>({
+// //     queryKey: ['getAllResume'],
+// //     queryFn: () => {
+// //       return !window ? [] : resumeService.getAllResume();
+// //     },
+// //   });
+// // };
